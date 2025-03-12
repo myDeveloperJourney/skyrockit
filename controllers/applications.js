9c793cae547bdd55881fbe5e4e28b3bff553a45e
Daniel Scott <daniel@mydeveloperjourney.com>
2025-03-12 17:26:15 -0500

    wip: completed index view & adds new functionality

--- controllers/applications.js
+++ controllers/applications.js
@@ -5,7 +5,31 @@ const User = require('../models/user');
 // GET /users/:userId/applications
 router.get('/', async (req, res) => {
     try {
-        res.render('applications/index.ejs');
+        const currentUser = await User.findById(req.session.user._id);
+
+        res.render('applications/index.ejs', {
+            applications: currentUser.applications
+        });
+        // pass the current user's applications to the index page
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+// GET /users/:userId/applications/new
+router.get('/new', async (req, res) => {
+    res.render('applications/new.ejs');
+});
+
+// POST /users/:userId/applications
+router.post('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.applications.push(req.body); 
+        // this changes the applications list in memory ONLY - NOT in the database
+        await currentUser.save(); // this makes the changes permanent in the database
+        res.redirect(`/users/${currentUser._id}/applications`);
     } catch (error) {
         console.log(error);
         res.redirect('/');
